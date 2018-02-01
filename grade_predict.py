aa84ed7e468af8020bb238290135de34d33e1d98
Jonathan Shi <shijo@umich.edu>
2018-02-01 09:57:14 -0500

    first

---
+++ grade_predict.py
@@ -0,0 +1,59 @@
+# discussion - 13 drop 2 total 110 points 10 each
+# homeworks 14 drop 2
+# lecture ex 26 drop 4
+# midetersm 2
+# projects 3
+# final projects
+# to do add points drop scores- sort, get data, convert to letter grade, convert to percentage
+
+# read in a file from csv
+def get_data():
+    pass
+
+# sorts scores in order highest to lowest in numerical value
+# param: list of sort_scores
+# return: sorted list of scores
+def sort_scores(scores):
+    pass
+
+# adds up the total lecture scores dropping 4
+# param: list of scores
+# return: total scores after dropping lowest 4
+def calculate_lecture_exercise_score(lecture_scores):
+    pass
+
+# adds up the total discussion scores dropping 2
+# param: list of scores
+# return: total scores after dropping lowest 2
+def calculate_discussion_score(discussion_scores):
+    pass
+
+# adds up the total homework scores dropping 2
+# param: list of scores
+# return: total scores after dropping lowest 2
+def calculate_homework_score(homework_scores):
+    pass
+
+# adds up the total exam scores
+# param: list of scores
+# return: total scores
+def calculate_midterm_score(midterm_scores):
+    pass
+
+# adds up the total project score
+# param: list of scores
+# return: total scores
+def calculate_project_score(project_scores):
+    pass
+
+# converts a total score into a percentage
+# param: total score, after dropping and applying policies
+# return: float representing a percentage 0,1
+def convert_to_percent(total_score):
+    pass
+
+# converts a percent to a letter grade
+# param: percentage score
+# return: string representing a letter grade
+def convert_to_letter_grade(total_percent):
+    pass
---
+++ ttt.py
@@ -0,0 +1,29 @@
+# print empty board
+# get input
+# updated board (save and display)
+# who won? what move?
+# make sure the move is valid
+
+# prints out the board to the screen
+# param: empty tic tac toe board
+# returns: nothing
+def print_board(board):
+    pass
+
+# determine whether or not the move is valid
+# param: board and the position of the move the user is trying to make
+# returns: a bool
+def is_valid_move(board, position):
+    return True
+
+# determine whether or not there is a winner ( 3 in a row)
+# param: board
+# returns: a bool
+def check_winner(board):
+    return False
+
+# updates the board according to the most recent is_valid_move
+# param: board
+# returns: the updated board
+def update_board(board):
+    pass
